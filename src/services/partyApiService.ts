6b86edfd841c500f9fd06f70a1a317d89ee61628
John Ferguson Smart <john.smart@wakaleo.com>
2025-04-06 13:34:53 +0100

    Updated APU url

--- src/services/partyApiService.ts
+++ src/services/partyApiService.ts
@@ -1,7 +1,7 @@
 // src/services/partyApiService.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5044/api';
 
 export interface Party {
     partyId: string;
